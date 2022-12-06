d8a77244b9b1169b801157490a225768ee3742cd
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-06 12:47:03 +0530

    function assignment

---
+++ week2-day2-6dec2022/js_basics/assignment_function.js
@@ -0,0 +1,14 @@
+
+// functon defn
+function subtract(a,b){
+  return a-b;
+}
+
+//function expresson
+let product = function(a,b){
+    return a*b;
+}
+
+
+console.log(subtract(2,3));
+console.log(product(2,3));
