4fe4e3e68848ea71192276e398e597556ace0e3c
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-06 14:07:10 +0530

    cart task

--- week2-day2-6dec2022/js_basics/assignment_function.js
+++ week2-day2-6dec2022/js_basics/assignment_function.js
@@ -9,6 +9,23 @@ let product = function(a,b){
     return a*b;
 }
 
-
 console.log(subtract(2,3));
 console.log(product(2,3));
+
+
+let cart = {
+    add: function(){
+        console.log("addding into cart");
+    },
+    edit: function(id){
+        console.log(`editing the cart ${id}`);
+    }
+}
+
+cart.delete = function(){
+    console.log("deleting product");
+}
+
+cart.add();
+cart.edit(5);
+cart.delete();
