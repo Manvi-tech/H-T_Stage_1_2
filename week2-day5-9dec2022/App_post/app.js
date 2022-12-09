7b0dfc4801e65e1c385a425623b84e585735389a
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-09 14:57:10 +0530

    get posts usiing xhr and fetch from data.txt

---
+++ week2-day5-9dec2022/App_post/app.js
@@ -0,0 +1,56 @@
+
+// getposts: fetch async
+async function displayPosts(){
+  let response = await fetch("data.txt");
+  let data = await response.json();
+  console.log(data);
+}
+displayPosts();
+
+// get posts: xhr
+let btn = document.getElementById("displayPosts");
+function getPosts(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'data.txt', true);
+    xhr.onprogress = function(){
+        console.log('onprogress', xhr.readyState);
+    }
+    xhr.onload = function(){
+        console.log('onload', xhr.readyState);
+        if(this.status===200){
+            console.log('posts are: ', this.responseText);
+            document.getElementById("posts").innerText = this.responseText;
+        }
+    }
+    xhr.onerror=function(){
+        console.log('onload', xhr.readyState);
+    }
+    xhr.send();
+}
+btn.addEventListener('click', getPosts);
+
+// add a post
+
+let newPost = {
+    "title": "New Post",
+    "body": "This is new  post"
+}
+
+// POST: async
+// async function addPost(){
+//     const response = await fetch("data.txt", {
+//         method: "post",
+//         headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(newPost)
+//       })
+//       try{
+//         let json = await response.json();
+//         console.log(json);
+//       }catch(err){
+//         console.log(err);
+//       }
+// }
+// addPost();
