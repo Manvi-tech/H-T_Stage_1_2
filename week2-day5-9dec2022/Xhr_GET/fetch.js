630f849aa845349fa37069e3976db85ed195745e
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-09 11:20:08 +0530

    async fetch

--- week2-day5-9dec2022/Xhr_GET/fetch.js
+++ week2-day5-9dec2022/Xhr_GET/fetch.js
@@ -5,3 +5,9 @@ fetch('https://jsonplaceholder.typicode.com/users/5')
 .then((msg)=>{return (msg.json())})
 .then((data)=>console.log(data))
 .catch((err)=>{console.log(err)});
+
+async function fetch2(){
+    let response = await fetch("https://jsonplaceholder.typicode.com/users/5");
+    let data = await response.json();
+    console.log(data);
+}
