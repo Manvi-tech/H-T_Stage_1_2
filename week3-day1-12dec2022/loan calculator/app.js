a5f68f537cb3e1261cb7d3a5c8b74fe11bb3206a
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-12 09:52:48 +0530

    string to int

--- week3-day1-12dec2022/loan calculator/app.js
+++ week3-day1-12dec2022/loan calculator/app.js
@@ -22,7 +22,8 @@ function calculateInterest(e){
     let total_interest = document.getElementById("total_interest");
     let monthly_interest = document.getElementById("monthly_interest");
 
-    total_payment.innerHTML=`<h1>total payment is: ${principal+SI}</h1>`;
+    let a = parseInt(principal) + parseInt(SI);
+    total_payment.innerHTML=`<h1>total payment is: ${a}</h1>`;
     total_interest.innerHTML=`<h1>total interest is : ${SI}`;
     let temp = SI/(12*time);
     monthly_interest.innerHTML=`<h1>monthly interest is ${temp}`;
