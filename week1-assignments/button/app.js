79c47ab541178f1662b5e3953bbeefaf1f9787c0
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-13 07:45:54 +0530

    web socket

--- week1-assignments/button/app.js
+++ week1-assignments/button/app.js
@@ -1,4 +1,16 @@
 
+
 let btn = document.getElementById("btn");
 
- 
+
+btn.addEventListener('click', (e)=>{
+
+    if(e.altKey){
+        console.log("hello");
+    }
+
+    if(e.shiftKey){
+        console.log('hey whats up');
+    }
+    
+});
