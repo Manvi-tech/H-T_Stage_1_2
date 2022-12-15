2dfb422ea870437fc26466bde147aa897c2aa264
Manvi-tech <bansalmanvi1409@gmail.com>
2022-12-15 10:32:00 +0530

    maps and function with generics n ts

--- demo_typescript/week3-day4-15dec2022/generics.js
+++ demo_typescript/week3-day4-15dec2022/generics.js
@@ -1,17 +1,38 @@
+//single param
 function f1(args) {
-    console.log(typeof args);
-    return args;
+    console.log(` ${typeof args} ${args}`);
 }
-// f1<boolean>(true);
-function f2(arg1, arg2, arg3) {
-    console.log(arg1, arg2, arg3);
+f1("hello leo");
+//add
+function f2(arg1, arg2) {
+    return arg1 + arg2;
 }
-// f2<number, string, boolean>(2, "leoo", false);
-var Person = /** @class */ (function () {
-    function Person(t) {
+f2(2, 5);
+//map class
+class MyMap {
+    constructor() {
+        this.display = () => {
+            console.log(this.m);
+        };
+        this.m = new Map();
+    }
+    add(key, val) {
+        this.m.set(key, val);
+    }
+}
+let obj1 = new MyMap();
+obj1.add(2, 3);
+obj1.display();
+let obj2 = new MyMap();
+obj2.add("manvi", 22);
+obj2.display();
+let obj3 = new MyMap();
+obj3.add("hello", true);
+obj3.display();
+class Person {
+    constructor(t) {
         this.name = t;
     }
-    return Person;
-}());
-var manvi = new Person('manvi');
-console.log(manvi.name);
+}
+let manvi = new Person('manvi');
+// console.log(manvi.name);
--- demo_typescript/week3-day4-15dec2022/generics.ts
+++ demo_typescript/week3-day4-15dec2022/generics.ts
@@ -1,14 +1,48 @@
 
-function f1<T>(args:T):T{
-    console.log(typeof args);
-    return args;
+//single param
+function f1<T>(args:T):void{
+    console.log(` ${typeof args} ${args}`);
 }
-// f1<boolean>(true);
+f1<string>("hello leo");
 
-function f2<T, a, B>(arg1:T, arg2:a, arg3: B):void{
-    console.log(arg1, arg2, arg3);
+//add
+function f2<T>(arg1, arg2):T{
+    return arg1+arg2;
 }
-// f2<number, string, boolean>(2, "leoo", false);
+f2<number>(2, 5);
+
+//map class
+
+class MyMap<K,V>{
+    private m: Map<K,V>;
+
+    constructor(){
+        this.m = new Map<K,V>();
+    }
+
+    add(key:K, val:V){
+        this.m.set(key, val);
+    }
+
+    display=()=>{
+        console.log(this.m);
+    }
+}
+
+let obj1 = new MyMap<number, number>();
+obj1.add(2,3);
+obj1.display();
+
+let obj2 = new MyMap<string, number>();
+obj2.add("manvi", 22);
+obj2.display();
+
+let obj3 = new MyMap<string, boolean>();
+obj3.add("hello", true);
+obj3.display();
+
+
+
 
 
 class Person<T>{
@@ -21,4 +55,4 @@ class Person<T>{
 }
 
 let manvi = new Person<string>('manvi');
-console.log(manvi.name);
+// console.log(manvi.name);
