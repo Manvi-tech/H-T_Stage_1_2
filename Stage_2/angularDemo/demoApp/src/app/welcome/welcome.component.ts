f02d0f1059336a4d08e4a167b9f1b443d288007f
Manvi-tech <bansalmanvi1409@gmail.com>
2023-01-05 10:23:20 +0530

    subject and observables

--- Stage_2/angularDemo/demoApp/src/app/welcome/welcome.component.ts
+++ Stage_2/angularDemo/demoApp/src/app/welcome/welcome.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { LoggingService } from 'shared/logging.service';
@@ -10,21 +11,37 @@ import { LoggingService } from 'shared/logging.service';
 export class WelcomeComponent implements OnInit{
 
 
-  constructor(private logService:LoggingService){}
+  constructor(private logService:LoggingService, private http: HttpClient){}
   
   ngOnInit(): void {
     console.log('subscribed values :-')
-    // const obs = new Observable((data)=> data.next(Math.random()));
-    // obs.subscribe((d)=>console.log(d));
-    // obs.subscribe((d)=>console.log(d));
 
+    // unicast observable: different data for multiple subscribers or observer
+    const obs = new Observable((data)=> data.next(Math.random()));
+    obs.subscribe((d)=>console.log(d));
+    obs.subscribe((d)=>console.log(d));
+
+    // Note: behavior subject is used to emit last or imeeediate value
+    // multicast observable: same data for multiple subscribers or observer
     const subject = new Subject();
-
     subject.subscribe(d => console.log(d));
     subject.subscribe(d => console.log(d));
-
     subject.next(Math.random());
+
+    // const data = this.http.get('/api/products');
+    // data.subscribe(d => console.log(d));
+    // data.subscribe(d => console.log(d));
+ 
+    const subject1 = new Subject();
+    const data = this.http.get('/api/products');
+    subject1.subscribe(d => console.log(d));
+    subject1.subscribe(d => console.log(d));
+    //data is observable so we have to subscribe to it 
+    const result = data.subscribe(subject1);
+    // subject1.next(data);
+    
   }
+
 
   pageTitle:string="title";
   data:number=3;
